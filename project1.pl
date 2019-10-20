9ab883fa63f2ec3e18a9d7bc35e939811b9fd25b
Goncalo101 <goncalo.oli@outlook.com>
2019-10-20 17:07:24 +0100

    Implemented display system

--- project1.pl
+++ project1.pl
@@ -5,16 +5,52 @@
 /* :- include('file.pl') */
 :- use_module(library(lists)).
 
-start:- initBoard(T), printBoard(T).
+start:- initBoard(T), transpose(T, Board), display_game(Board, _).
 
 initBoard(T) :-
-    append([[
-        [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
-        [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
-        [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
-        [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]]]], T).
-    /*append([[[2,2,2,2]], [[0,0,0,0]], [[0,0,0,0]], [[1,1,1,1]]], T).*/
-    
+    append([[[[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
+            [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
+            [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]],
+            [[2,2,2,2], [0,0,0,0], [0,0,0,0], [1,1,1,1]]]], T).
+
+translate(0, ' ').
+translate(1, 'X').
+translate(2, 'O').
+
+printCell(C) :-
+    translate(C,P),
+    write(P).
+
+printLine([]) :-
+    write(' ').
+
+printLine([C|L]):-
+    printCell(C),
+    printLine(L).
+
+printSep(0).
+printSep(1).
+printSep(2) :- write(' '), write(' ').
+printSep(3).
+printSep(4).
+
+printBoard([], 0).
+printBoard([Line|Lines], N) :-
+    N > 0,
+    N1 is N - 1,
+    printSep(N),
+    printLine(Line),
+    printBoard(Lines, N1).
+
+printBoards([], 0).
+printBoards([Tab|Tabs]) :-
+    printBoard(Tab, 4),
+    nl,
+    printBoards(Tabs).
+
+display_game(Board, Player) :-
+    printBoards(Board).
+
 
 /*
 translate(1, 'X').

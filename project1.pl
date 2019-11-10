440a304d31e1407519e8fdc1934ad97b3364db5f
Goncalo101 <goncalo.oli@outlook.com>
2019-11-10 11:38:14 +0000

    Moved notes away from project files

--- project1.pl
+++ project1.pl
@@ -1,8 +1,3 @@
-/* usar nl para newline e predicados psep e pvsep para separadores */
-
-/* §10.22 in sisctus prolog docs: https://sicstus.sics.se/sicstus/docs/latest4/html/sicstus.html/lib_002dlists.html#lib_002dlists */
-/* ver nth0, maplist, remove_dups, sumlist, findall, bagof, setof */
-/* :- include('file.pl') */
 :- use_module(library(lists)).
 
 start:- initBoard(T), transpose(T, Board), display_game(Board, _).
@@ -56,24 +51,3 @@ display_game(Board, Player) :-
     write(' -------------- --------------    -------------- -------------- '), nl,
     printBoards(Board),
     write(' -------------- --------------    -------------- -------------- ').
-
-/*
-translate(1, 'X').
-printCell(C) :-
-    translate(C,P),
-    write(P).
-printLine([]).
-printLine([C|L]):-
-    printCell(C),
-    printLine(L).
-printBoard([]).
-printBoard([L|T]) :-
-    printLine(L),
-    printBoard(T).
-    */
-
-/* nivel 1 da ia (exemplo) 
-findall(X-Y, validMove(Tab, X,Y, NTab), L). */
-
-/* nivel 2 da ia (exemplo)
-setof(V-X-Y, (validMove(Tab, X,Y, NTab), eval(NTab, V)), [Play|_]). */

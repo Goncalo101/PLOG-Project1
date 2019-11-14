eba77c1065534c94ceb152f4832b47e9952a71a4
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-11-14 11:21:08 +0000

    Restructred game.pl

--- game.pl
+++ game.pl
@@ -1,22 +1,28 @@
 :- include('board.pl').
 :- include('menu.pl').
 
-/* Create a Move*/
 play:-
     initialMenuHandler.
 
+/* Create a Move*/
 start :-
     start(Board),
     write('Player with X starts!'), nl,
     write('Passive move'), nl,
     getBoardNumber(BoardNo),
-    write('Origin coordinates'), nl,
-    getOriginLine(OLine),
-    getOriginColumn(OColumn), nl,
-    write('Destination coordinates'), nl,
-    getDestinationLine(DLine),
-    getDestinationColumn(DColumn),
+    getOriginCoordinates(OLine, OColumn),
+    getDestinationCoordinates(DLine, DColumn),
     setPiece(1, BoardNo, Board, DLine, DColumn, Board2),
     setPiece(0, BoardNo, Board2, OLine, OColumn, FollowingBoard),
     displayGame(FollowingBoard),
     write('Done!').
+
+getDestinationCoordinates(DLine, DColumn) :-
+    write('Destination coordinates'), nl,
+    getDestinationLine(DLine),
+    getDestinationColumn(DColumn).
+
+getOriginCoordinates(OLine, OColumn) :-
+    write('Origin coordinates'), nl,
+    getOriginLine(OLine),
+    getOriginColumn(OColumn).

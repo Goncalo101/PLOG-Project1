2ecc25110bc42a7bf7b2ddeacd1dd939b1e8c4f6
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-11-17 23:03:41 +0000

    Removed test predicate

--- game.pl
+++ game.pl
@@ -344,9 +344,4 @@ calculatePossiblePlay(Player, Board, [H1|[H2|[H3|T]]], NewRow, NewCol, ArrayMove
 valid_moves(Player, Board, ListOfMoves) :-
     getAllPlayerPiecesPosition(Player, Board, Row, Column, ListOfPositions),
     passiveMovesAvailable(Player, Board, Row, COlumn, ListOfPositions).
-
-teste:-
-    initBoard(Board),
-    getAllPlayerPiecesPosition(1, Board, Row, Column, ListOfPositions),
-    passiveMovesAvailable(1, Board, ListOfPositions, ListWithMoves),
-    write(ListWithMoves).
+    
